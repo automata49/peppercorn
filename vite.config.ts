4fd38d705bed2bc73c8738304e90741b476a1579
automata49 <ky0409@gmail.com>
2026-09-26 08:58:16 +0900

    Fix Vite config for CI typecheck

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/peppercorn/' : '/',
+  base: '/peppercorn/',
   build: { sourcemap: true }
 })
